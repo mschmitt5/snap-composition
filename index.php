735d86b105390dd7cccf91d19a7e962023a51e99
Mary <schmitt.mary7@gmail.com>
2018-01-31 08:32:22 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: schmi
+ * Date: 1/31/2018
+ * Time: 8:25 AM
+ **/
+
+class car {
+    protected $wheels;
+    protected $body
+}
+
+trait brake {
+    public function stop(){
+        echo "Watch where you're going!";
+    }
+}
+
+class sedan extends car {
+    protected $fourDoors;
+    use brake;
+}
+
+class twoDoor extends car {
+    use brake;
+}
